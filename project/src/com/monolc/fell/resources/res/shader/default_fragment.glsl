46cbacfa6a446f77ce4ccf9254c3fe5dd444cb72
nulltitleexception <clbprfftt@gmail.com>
2015-02-28 18:16:12 -0500

    fixed weird conflicts thing
    ^ that

--- project/src/com/monolc/fell/resources/res/shader/default_fragment.glsl
+++ project/src/com/monolc/fell/resources/res/shader/default_fragment.glsl
@@ -5,9 +5,5 @@ in vec3 vertexColor;
 out vec4 fragColor;
 
 void main() {
-<<<<<<< HEAD
     fragColor = vec4(vertexColor, 1.0);
-=======
-    fragColor = vec4(vertexColor,1.0);
->>>>>>> origin/master
 }
